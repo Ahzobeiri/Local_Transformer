8db3c231428f02fae2b98f76b0ad4d46b63b657e
Amirhosein Zobeiri <127421619+Ahzobeiri@users.noreply.github.com>
2025-03-19 20:40:24 +0330

    Update team_code_Cbramod.py

--- team_code_Cbramod.py
+++ team_code_Cbramod.py
@@ -13,6 +13,10 @@ from helper_code import *
 import numpy as np, os, sys
 import mne
 import joblib
+import lmdb
+import pickle
+from tqdm import tqdm
+
 
 
 import os
@@ -192,6 +196,38 @@ def get_eeg(data_folder, patient_id):
 
 
 
+# Preprocess data.
+def preprocess_data(data, sampling_frequency, utility_frequency):
+    # Define the bandpass frequencies.
+    passband = [0.1, 30.0]
+
+    # Promote the data to double precision because these libraries expect double precision.
+    data = np.asarray(data, dtype=np.float64)
+
+    # If the utility frequency is between bandpass frequencies, then apply a notch filter.
+    if utility_frequency is not None and passband[0] <= utility_frequency <= passband[1]:
+        data = mne.filter.notch_filter(data, sampling_frequency, utility_frequency, n_jobs=4, verbose='error')
+
+    # Apply a bandpass filter.
+    data = mne.filter.filter_data(data, sampling_frequency, passband[0], passband[1], n_jobs=4, verbose='error')
+
+    # Resample the data
+    resampling_frequency = 128  # Change based on the desired resampling_frequency
+    lcm = np.lcm(int(round(sampling_frequency)), int(round(resampling_frequency)))
+    up = int(round(lcm / sampling_frequency))
+    down = int(round(lcm / resampling_frequency))
+    data = scipy.signal.resample_poly(data, up, down, axis=1)
+
+    # Scale the data to the interval [-1, 1].
+    min_value = np.min(data)
+    max_value = np.max(data)
+    if min_value != max_value:
+        data = 2.0 / (max_value - min_value) * (data - 0.5 * (min_value + max_value))
+    else:
+        data = 0 * data
+
+    return data, resampling_frequency
+
 
 
 
@@ -321,41 +357,7 @@ def save_challenge_model(model_folder, imputer, outcome_model, cpc_model):
     joblib.dump(d, filename, protocol=0)
 
 
-# Preprocess data.
-def preprocess_data(data, sampling_frequency, utility_frequency):
-    # Define the bandpass frequencies.
-    passband = [0.1, 30.0]
 
-    # Promote the data to double precision because these libraries expect double precision.
-    data = np.asarray(data, dtype=np.float64)
-
-    # If the utility frequency is between bandpass frequencies, then apply a notch filter.
-    if utility_frequency is not None and passband[0] <= utility_frequency <= passband[1]:
-        data = mne.filter.notch_filter(data, sampling_frequency, utility_frequency, n_jobs=4, verbose='error')
-
-    # Apply a bandpass filter.
-    data = mne.filter.filter_data(data, sampling_frequency, passband[0], passband[1], n_jobs=4, verbose='error')
-
-    # Resample the data.
-    if sampling_frequency % 2 == 0:
-        resampling_frequency = 128
-    else:
-        resampling_frequency = 125
-    lcm = np.lcm(int(round(sampling_frequency)), int(round(resampling_frequency)))
-    up = int(round(lcm / sampling_frequency))
-    down = int(round(lcm / resampling_frequency))
-    resampling_frequency = sampling_frequency * up / down
-    data = scipy.signal.resample_poly(data, up, down, axis=1)
-
-    # Scale the data to the interval [-1, 1].
-    min_value = np.min(data)
-    max_value = np.max(data)
-    if min_value != max_value:
-        data = 2.0 / (max_value - min_value) * (data - 0.5 * (min_value + max_value))
-    else:
-        data = 0 * data
-
-    return data, resampling_frequency
 
 
 # Extract features.

b4d93a4f706e486b0ed062b23177a35267774807
Amirhosein Zobeiri <127421619+Ahzobeiri@users.noreply.github.com>
2025-04-03 16:22:56 +0330

    Update Run_custom_finetune_main.py

--- Run_custom_finetune_main.py
+++ Run_custom_finetune_main.py
@@ -1,1 +1,2 @@
-python custom_finetune_main.py --downstream_dataset I-CARE-CPC --datasets_dir /data/datasets/BigDownstream/I-CARE/LMDB_DATA --num_of_classes 5 --model_dir .../models/I-care --epochs 50 --batch_size 64 --lr 5e-4
+python custom_finetune_main.py --downstream_dataset I-CARE-CPC --datasets_dir /projects/scratch/fhajati/physionet.org/files/i-care/2.1/LMDB_DATA --num_of_classes 5 --model_dir /projects/scratch/fhajati/physionet.org/Github_Co
+des/python-example-2023/models/I-CARE --epochs 50 --batch_size 60 --lr 5e-4
